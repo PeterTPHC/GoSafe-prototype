bd7d7534ecbd15e4b1a8039fa2901cee033713a0
Peter Hut <peter@tphc.nl>
2026-09-01 09:37:24 +0200

    Fix admin permissions initialization

--- assets/communications.js
+++ assets/communications.js
@@ -2,5 +2,5 @@ const communicationModule=document.createElement('script');
 communicationModule.src='assets/communications-core.js?v=20260901-users-rights-v1';
 document.body.appendChild(communicationModule);
 const usersModule=document.createElement('script');
-usersModule.src='assets/users.js?v=20260901-users-rights-v1';
+usersModule.src='assets/users.js?v=20260901-users-rights-v2';
 document.body.appendChild(usersModule);
